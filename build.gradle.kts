d0300dcf2f3aba3f4471e40792b9e982fcd3d5e7
Fernando Cejas <fcejas@gmail.com>
2021-02-28 15:16:59 +0100

    Updated last commit since it was failing.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 
     // Internal Script plugins
     id(ScriptPlugins.compilation)
+    id(ScriptPlugins.infrastructure)
 }
 
 repositories {
@@ -12,18 +13,20 @@ repositories {
     mavenCentral()
 }
 
+group = "com.fernandocejas.restapi"
+version = "1.0-SNAPSHOT"
+application { mainClass.set("io.ktor.server.netty.EngineMain") }
+
 dependencies {
+    // Application dependencies
     implementation(kotlin("stdlib"))
     implementation("io.ktor:ktor-server-core:1.5.2")
     implementation("io.ktor:ktor-server-netty:1.5.2")
     implementation("ch.qos.logback:logback-classic:1.2.3")
 
+    // Unit/Android tests dependencies
+    testImplementation(TestLibraries.junit4)
+    testImplementation(TestLibraries.mockk)
+    testImplementation(TestLibraries.kluent)
     testImplementation("io.ktor:ktor-server-tests:1.5.2")
-}
-
-group = "com.fernandocejas.restapi"
-version = "1.0-SNAPSHOT"
-
-application {
-    mainClass.set("io.ktor.server.netty.EngineMain")
 }
--- buildSrc/src/main/kotlin/scripts/infrastructure.gradle.kts
+++ buildSrc/src/main/kotlin/scripts/infrastructure.gradle.kts
@@ -15,10 +15,6 @@
  */
 package scripts
 
-tasks.register("clean", Delete::class){
-    delete(rootProject.buildDir)
-}
-
 //tasks.named<Wrapper>("wrapper") {
 //    gradleVersion = BuildPlugins.Versions.gradleVersion
 //    distributionType = Wrapper.DistributionType.ALL
